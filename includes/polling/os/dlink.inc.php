a8b4a181a36abfa700eeb0df6893d8eadf33d97d
Michael Manerko <asper@tagan.ru>
2019-12-17 09:17:19 +0300

    d DGS 1100-10/ME

--- includes/polling/os/dlink.inc.php
+++ includes/polling/os/dlink.inc.php
@@ -9,6 +9,7 @@ $versionOIDList = [
      'DLINKSW-ENTITY-EXT-MIB::dEntityExtVersionRuntime.1',
      '.1.3.6.1.2.1.16.19.2.0',
      '.1.3.6.1.4.1.171.12.11.1.9.4.1.11.1',
+     '.1.3.6.1.4.1.171.10.134.2.1.1.1.3.0',
 ];
 
 foreach ($versionOIDList as $oid) {
@@ -23,6 +24,7 @@ foreach ($versionOIDList as $oid) {
 $serialOIDList = [
      '.1.3.6.1.4.1.171.12.11.1.9.4.1.17.1',
      '.1.3.6.1.4.1.171.12.1.1.12.0',
+     '.1.3.6.1.4.1.171.10.134.2.1.1.29.0',
 ];
 
 foreach ($serialOIDList as $oid) {
